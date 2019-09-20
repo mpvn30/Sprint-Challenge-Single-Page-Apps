805166acf49dbccdf3ce6775b0aeee3265c2f057
Mike Nguyen <mpvnguyen1996@gmail.com>
2019-09-20 11:35:19 -0400

    finished step 1 + routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
+import CharacterList from "./components/CharacterList"
 import Header from "./components/Header.js";
+import { Route } from 'react-router-dom';
+import WelcomePage from "./components/WelcomePage";
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      <Route exact path="/" component={WelcomePage} />
+      <Route path="/characters" component={CharacterList} />
     </main>
   );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,52 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios'
+import {
+  Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button
+} from 'reactstrap';
 
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
-
+  const [ characters, setCharacter ] = useState([]);
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    const getCharacter = () => {
+      axios
+        .get('https://rickandmortyapi.com/api/character/')
+        .then(response => {
+          setCharacter(response.data.results);
+          console.log(response.data.results)
+        })
+        .catch(error => {
+          console.error('Server Error', error);
+        });
+    }
+    
+    getCharacter();
   }, []);
+ 
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {characters.map(character =>(
+        <CharacterDetails key={character.id} character={character}/>
+      ))}
     </section>
   );
 }
+
+function CharacterDetails({ character }){
+  const { name, image, species, status } = character;
+  return(
+      <Card>
+        <CardImg top width="100%" src={image} alt="Card image cap" />
+        <CardBody>
+          <CardTitle>{name}</CardTitle>
+          <CardSubtitle>Species: {species}</CardSubtitle>
+          <CardSubtitle>Status: {status}</CardSubtitle>
+        </CardBody>
+      </Card>
+  )
+}
+
+ // TODO: Add useState to track data from useEffect
+    // TODO: Add API Request here - must run in `useEffect`
+    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
